01042b8555271b1e6cb10f776b1e7faae44c44c6
margox <margox@foxmail.com>
2016-03-04 21:46:39 +0800

    N/A

--- src/view/assets/js/app.js
+++ src/view/assets/js/app.js
@@ -9,4 +9,8 @@ window.miniPlayer = function () {
         localStorage['__mini_player_list__'] = JSON.stringify(playlist);
     });
 
+    // TODO 将player模块分离出来，作为audio对象的扩展，扩展功能如下：
+    // 1.支持列表
+    // 2.播放模式
+
 }();
