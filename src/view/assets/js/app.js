59f38ae6560037b4b47f490805bfd4b7ccb96dac
margox <margox@foxmail.com>
2016-03-05 22:42:01 +0800

    代码重构版本

--- src/view/assets/js/app.js
+++ src/view/assets/js/app.js
@@ -3,14 +3,9 @@ window.miniPlayer = function () {
     'use strict';
 
     var playlist = localStorage['__mini_player_list__'] ? JSON.parse(localStorage['__mini_player_list__']) : [];
-    var player = new Player(playlist);
-    var i = 0;
-    player.vue.$on('playlistchanged', function(playlist) {
-        localStorage['__mini_player_list__'] = JSON.stringify(playlist);
+    Player.list(playlist);
+    Player.on('listchange', function(list) {
+        localStorage['__mini_player_list__'] = JSON.stringify(list);
     });
-
-    // TODO 将player模块分离出来，作为audio对象的扩展，扩展功能如下：
-    // 1.支持列表
-    // 2.播放模式
 
 }();
