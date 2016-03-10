0cddb0525d549ad2085bd295f8f0791a570098c7
margox <margox@foxmail.com>
2016-03-10 12:27:13 +0800

    Update app.js

--- src/view/assets/js/app.js
+++ src/view/assets/js/app.js
@@ -1,10 +1,9 @@
-window.miniPlayer = function () {
+~function () {
 
     'use strict';
 
     var playlist = localStorage['__mini_player_list__'] ? JSON.parse(localStorage['__mini_player_list__']) : [];
-    Player.list(playlist);
-    Player.on('listchange', function(list) {
+    Player.list(playlist).on('listchange', function(list) {
         localStorage['__mini_player_list__'] = JSON.stringify(list);
     });
 
